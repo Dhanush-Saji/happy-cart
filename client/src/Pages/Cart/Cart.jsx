74f3abcd0425464ee2b8a08b0fc5e8c41bb06e0f
Dhanush Saji <dhanushsaji1@gmail.com>
2023-03-05 10:32:57 +0530

    frontend structure completed

--- client/src/Pages/Cart/Cart.jsx
+++ client/src/Pages/Cart/Cart.jsx
@@ -1,6 +1,43 @@
 import "./Cart.scss";
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
+import CartItem from "./CartItem/CartItem";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+
 const Cart = () => {
-    return <div>Cart</div>;
+  return (
+    <div className="cart-panel">
+      <div className="cart-content">
+        {/* <div className="empty-cart">
+          <BsCartX />
+          <span>No products in the cart</span>
+          <button className="return-cta">RETURN TO SHOP</button>
+        </div> */}
+        <>
+          <CartItem />
+          <div className="cart-footer">
+            <div className="subtotal">
+              <span className="text">Subtotal:</span>
+              <span className="text total">₹451</span>
+            </div>
+              <div className="button">
+                <Button w='100%' bg={'#8e2de2'} _hover={{bg:'#6d1eb3'}} color='white'>Checkout</Button>
+              </div>
+          </div>
+        </>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ChakraProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </ChakraProvider>
 );
 
 reportWebVitals();
